c20c5aba4e9a1e6e4eb2c08a62214dfab4138772
Jacob Galam <hgecdkto@gmail.com>
2020-08-09 13:10:05 +0300

    make the numbers generator in the interface class

--- add_test.py
+++ add_test.py
@@ -1,8 +1,6 @@
-from typing import Tuple
 import game_modeI
 from difficulty import Difficulty
 from question import Question
-from generator import Generator
 
 
 class AddTest(game_modeI.GameModeI):
@@ -16,19 +14,3 @@ class AddTest(game_modeI.GameModeI):
         question = f"What is {numbers[0]} + {numbers[1]}?"
 
         return Question(question, sum(numbers))
-
-    @staticmethod
-    def generate_numbers(difficulty) -> Tuple[int, int]:
-        num_of_digits = AddTest.get_num_digits(difficulty)
-        first_num = Generator.positive_num(num_of_digits)
-        second_num = Generator.positive_num(num_of_digits)
-        return first_num, second_num
-
-    @staticmethod
-    def get_num_digits(difficulty: Difficulty) -> int:
-        if difficulty == difficulty.EASY:
-            return 1
-        if difficulty == difficulty.MEDIUM:
-            return 3
-        if difficulty == difficulty.HARD:
-            return 7
--- game_modeI.py
+++ game_modeI.py
@@ -1,5 +1,8 @@
+from typing import Tuple
 from difficulty import Difficulty
 from question import Question
+from generator import Generator
+
 
 class GameModeI:
 
@@ -9,3 +12,18 @@ class GameModeI:
     def get_question(self, difficulty: Difficulty) -> Question:
         pass
 
+    @staticmethod
+    def generate_numbers(difficulty) -> Tuple[int, int]:
+        num_of_digits = GameModeI.get_num_digits(difficulty)
+        first_num = Generator.positive_num(num_of_digits)
+        second_num = Generator.positive_num(num_of_digits)
+        return first_num, second_num
+
+    @staticmethod
+    def get_num_digits(difficulty: Difficulty) -> int:
+        if difficulty == difficulty.EASY:
+            return 1
+        if difficulty == difficulty.MEDIUM:
+            return 3
+        if difficulty == difficulty.HARD:
+            return 7
