f534b4f124b367423c2c34f14be53e426020ffa7
Jacob Galam <hgecdkto@gmail.com>
2020-08-09 03:06:20 +0300

    change prints

--- add_test.py
+++ add_test.py
@@ -1,22 +1,24 @@
+from typing import Tuple
 import game_modeI
 from difficulty import Difficulty
 from question import Question
 from generator import Generator
 
+
 class AddTest(game_modeI.GameModeI):
 
     def get_description(self) -> str:
-        return "sum positive"
+        return "Positive plus"
 
     def get_question(self, difficulty: Difficulty) -> Question:
         numbers = self.generate_numbers(difficulty)
 
-        question = f"what is {numbers[0]} + {numbers[1]}?"
+        question = f"What is {numbers[0]} + {numbers[1]}?"
 
         return Question(question, sum(numbers))
 
     @staticmethod
-    def generate_numbers(difficulty):
+    def generate_numbers(difficulty) -> Tuple[int, int]:
         num_of_digits = AddTest.get_num_digits(difficulty)
         first_num = Generator.positive_num(num_of_digits)
         second_num = Generator.positive_num(num_of_digits)
