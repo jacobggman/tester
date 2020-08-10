99daf1bd3328373e5884325aa788f457d8a2d339
Jacob Galam <hgecdkto@gmail.com>
2020-08-10 19:42:26 +0300

    make the numbers divide without a remainder

--- tests_objects/division.py
+++ tests_objects/division.py
@@ -8,11 +8,16 @@ class DivisionTest(game_modeI.TestI):
     def get_description(self) -> str:
         return "Positive division"
 
+    def get_divide_no_remainder_numbers(self, difficulty):
+        while True:
+            numbers = self.generate_numbers(difficulty)
+            if numbers[0] % numbers[1] == 0:
+                return numbers
+
     def get_question(self, difficulty: Difficulty) -> Question:
-        numbers = self.generate_numbers(difficulty)
+        numbers = self.get_divide_no_remainder_numbers(difficulty)
+        question = f"What is {numbers[0]} / {numbers[1]}?"
 
-        question = f"What is {numbers[0]} / {numbers[1]}? (two numbers after comma)"
-
-        answer = numbers[0] / numbers[1]
+        answer = numbers[0] // numbers[1]
         answer = round(answer, 2)
         return Question(question, answer)
